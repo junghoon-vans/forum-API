d0885c9f7082a890c34c29100050ee70324dd1b8
Jeonghun-Ban <bjh5230@gmail.com>
2020-03-17 15:58:49 +0900

    Connect to postgreSQL DB

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from app.config import config_by_name
+from flask_sqlalchemy import SQLAlchemy
+
+from app.config import mode
+from app.models import db
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config_by_name[config_name])
+    app.config.from_object(mode[config_name])
+    app.app_context().push()
+    db.init_app(app)
+    db.create_all()
     return app
--- flask/app/config.py
+++ flask/app/config.py
@@ -1,22 +1,22 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-# postgres_local_base = os.environ['DATABASE_URL']
+postgres_local_base = os.environ['DATABASE_URL']
 
 
 class Config:
 	SECRET_KEY = os.urandom(16)
 	DEBUG = False
-
+	SQLALCHEMY_DATABASE_URI = postgres_local_base
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
 	DEBUG = True
 
 class ProductionConfig(Config):
 	DEBUG = False
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
 
 
-config_by_name = dict(
+mode = dict(
 	dev=DevelopmentConfig,
 	prod=ProductionConfig
 )
