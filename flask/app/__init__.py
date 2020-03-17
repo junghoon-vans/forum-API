6087b7212dc2be8f8179a0ef683b089258b420d9
Jeonghun-Ban <bjh5230@gmail.com>
2020-03-18 00:24:11 +0900

    Apply flask-restplus to use Swagger

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_restplus import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
 from app.config import mode
@@ -8,5 +9,12 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(mode[config_name])
     app.app_context().push()
+
+    api = Api(app, 
+          version='1.0',
+          title='Forum API',
+          description='flask-based bulletin board service API',
+          )
+
     db.init_app(app)
     return app
