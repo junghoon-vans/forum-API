0bfbd4a1032b4a9be54624911a94152c50d95f99
Jeonghun-Ban <bjh5230@gmail.com>
2020-03-19 12:43:09 +0900

    Make session class in redis

--- flask/src/utils/redis.py
+++ flask/src/utils/redis.py
@@ -1,4 +1,22 @@
 from redis import Redis
+from uuid import uuid4
 
 
-redis = Redis(os.environ['REDIS_URL'])
+class session:
+    prefix = 'session_key:'
+    host = os.environ['REDIS_URL']
+    timeout = 3600
+
+    def __init__(self):
+        self.db = Redis(host)
+
+    def create(self, user_name):
+        session_key = str(uuid4())
+        self.db.setex(self.prefix+session_key, user_name, self.timeout)
+
+    def open(self, session_key):
+        user_name = self.db.get(self.prefix+session_key)
+
+        if user_name is not None:
+            self.db.expire(self.prefix+session_key, self.timeout)
+        return user_name
