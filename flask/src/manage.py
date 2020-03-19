6eeacc0c15d56cb9a3dbe84c2987151d70a4a8a7
Jeonghun-Ban <bjh5230@gmail.com>
2020-03-20 03:17:27 +0900

    Update manage.py to support automatic-migrations

--- flask/src/manage.py
+++ flask/src/manage.py
@@ -3,9 +3,13 @@ import os
 from flask_script import Manager
 
 from app import create_app
+from utils.sqlalchemy import Base, engine
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'prod')
 manager = Manager(app)
+
+Base.metadata.create_all(engine)
 
 @manager.command
 def run():
